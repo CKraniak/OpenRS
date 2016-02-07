b8d8793f685285b1260b1bf0218126a8d84288c5
CKraniak <ckraniak@live.com>
2016-02-07 00:26:19 -0600

    Text display mostly functional
    The main change is that now I have the window text
    displaying properly. I am also able to grab an
    arbitrary font; it'll be Courier New for now, mostly
    for the monospace. Next thing will be to make the
    string it displays be created by the
    AsciiDisplaySystem.
    
    The other thing I've done is make the window not
    resizable. This is just so I don't have to resize
    the text or other nonsense.
    
    Signed-off-by: CKraniak <ckraniak@live.com>

--- 0-9-0-2/main.cpp
+++ 0-9-0-2/main.cpp
@@ -1,36 +1,128 @@
 #include <iostream>
+#include <sstream>
 
 #include <windows.h>
+#include "strsafe.h"
 
 #include "utility.h"
+#include "dispatcher/dispatcher.h"
 
-bool quit;
-
-//#include "dispatcher/dispatcher.h"
+// Put this AFTER all includes
+// Can't pollute other files if other files never see this crap.
+static void * global_addr = nullptr;
 
 // Class to give main the interface it needs to other pieces.
-//struct InputData;
+class MLInterface {
+    bool quit_;
+    HWND h_wnd_;
+    ENUMLOGFONTEXDV * ptr_elf;
 
-//class MLInterface {
-//    InputData data;
+public:
+    MLInterface() : quit_(false) {}
+    void update();
+    void render();
+    static LRESULT CALLBACK MainWndProc(HWND, UINT, WPARAM, LPARAM);
+    void setQuit(bool q = true) { quit_ = q; }
+    bool shouldQuit() { return quit_; }
+    void emptyMessagePump();
+    bool createMainWindow(HINSTANCE, int);
+};
 
-//public:
-//    typedef enum {
-//        RECEIVED,
-//        EXIT = 0,
-//        NONE
-//    } INPUT_STATUS;
+void MLInterface::emptyMessagePump() {
+    MSG msg;
+    while(PeekMessage(&msg, h_wnd_, 0, 0, PM_REMOVE)) {
+        if(msg.message == WM_DESTROY) {
+            setQuit();
+        }
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+}
 
-//    void update();
-//    void render();
-//    INPUT_STATUS checkForInput();
-//    void dispatchInput(InputData data);
-//};
+bool MLInterface::createMainWindow(HINSTANCE h_inst,
+                                   int cmd_show_opt) {
+    WNDCLASSEX wc;
+    wc.cbSize           = sizeof(WNDCLASSEX);
+    wc.style            = 0;
+    wc.lpfnWndProc      = MLInterface::MainWndProc;
+    wc.cbClsExtra       = 0;
+    wc.cbWndExtra       = 0;
+    wc.hInstance        = h_inst;
+    wc.hIcon            = NULL;
+    wc.hCursor          = NULL;
+    wc.hbrBackground    = NULL;
+    wc.lpszClassName    = "ORS_MAIN_WINDOW_CLASS";
+    wc.lpszMenuName     = NULL;
+    wc.hIconSm          = NULL;
+    ATOM a = RegisterClassEx(&wc);
+    if(a == 0) {
+        ERR_MSGOUT_GLE("MLInterface::createMainWindow RegisterClassEx failed");
+        return false;
+    }
+    HWND h_wnd = CreateWindowEx(0,
+                                "ORS_MAIN_WINDOW_CLASS",
+                                "Test",
+                                WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME,// no resize
+                                CW_USEDEFAULT, CW_USEDEFAULT,
+                                640, 480,
+                                NULL, NULL,
+                                h_inst,
+                                NULL);
+    if(h_wnd == NULL) {
+        ERR_MSGOUT("MLInterface::createMainWindow CreateWindowEx failed");
+        return false;
+    }
+    // The return value of ShowWindow does not have meaning here.
+    ShowWindow(h_wnd, cmd_show_opt);
+    if (! UpdateWindow(h_wnd)) {
+        ERR_MSGOUT("MLInterface::createMainWindow UpdateWindow failed");
+        return false;
+    }
+    h_wnd_ = h_wnd;
+    return true;
+}
 
-LRESULT CALLBACK MainWndProc(HWND h_wnd,
-                             UINT msg,
-                             WPARAM w_param,
-                             LPARAM l_param) {
+void draw(HDC dc) {
+    if(dc == NULL) {
+        ERR_MSGOUT("Unable to get device context");
+        return;
+    }
+    RECT r;
+    GetClientRect(WindowFromDC(dc), &r);
+
+    FillRect(dc, &r, (HBRUSH)(COLOR_WINDOW + 1));
+
+    LOGFONT lf; // <-- parentheses means actually initialize, I believe.
+    memset(&lf, 0, sizeof(lf)); // but set the bitch anyway.
+//        lf.lfWeight = 900; // BOLD
+    strcpy_s(lf.lfFaceName, "Courier New");
+    lf.lfHeight = -24;
+    lf.lfWeight = FW_HEAVY;
+    HFONT test_font = CreateFontIndirect(&lf);
+    HFONT font;
+    if(test_font == NULL) {
+        font = *static_cast<HFONT*>(GetStockObject(ANSI_FIXED_FONT));
+    }
+    else {
+        font = test_font;
+    }
+
+    // Switch to Courier-style monospaced font
+    HFONT old_font = static_cast<HFONT>(SelectObject(dc, font));
+    if (old_font == NULL) {
+        ERR_MSGOUT("Failed to select font");
+    }
+    std::string s = "TESTING FONT, Courier New 012345 ilI1\niiiiiiiiiiiiii";
+    DrawText(dc, s.c_str(), s.size(), &r, DT_NOCLIP);
+    SelectObject(dc, old_font);
+}
+
+LRESULT CALLBACK MLInterface::MainWndProc(HWND h_wnd,
+                                          UINT msg,
+                                          WPARAM w_param,
+                                          LPARAM l_param) {
+    MLInterface * this_ = static_cast<MLInterface *>(global_addr);
+
     switch (msg) {
     case WM_CREATE:
         return 0;
@@ -46,10 +138,6 @@ LRESULT CALLBACK MainWndProc(HWND h_wnd,
         case VK_NUMPAD7:
         case VK_NUMPAD8:
         case VK_NUMPAD9:
-//            std::cout << "Numpad " <<
-//                         w_param - VK_NUMPAD0 <<
-//                         " pressed" <<
-//                         std::endl;
             break;
 
         default:
@@ -60,12 +148,14 @@ LRESULT CALLBACK MainWndProc(HWND h_wnd,
     case WM_PAINT: {
         PAINTSTRUCT ps;
         HDC dc = BeginPaint(h_wnd, &ps);
-        FillRect(dc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+        draw(dc);
         EndPaint(h_wnd, &ps);
-        ReleaseDC(h_wnd, dc);
         return 0;
     }
 
+    case WM_PRINTCLIENT: {
+        draw(reinterpret_cast<HDC>(w_param));
+    }
 
     case WM_CHAR:
         if(w_param == VK_ESCAPE) {
@@ -79,7 +169,7 @@ LRESULT CALLBACK MainWndProc(HWND h_wnd,
 
     case WM_DESTROY:
         PostQuitMessage(EXIT_SUCCESS);
-        quit = true;
+        this_->setQuit();
         break;
 
     default:
@@ -94,55 +184,13 @@ int WINAPI WinMain(HINSTANCE h_inst,
                    LPSTR cmd_args,
                    int cmd_show_opt)
 {
-    WNDCLASSEX wc;
-    wc.cbSize           = sizeof(WNDCLASSEX);
-    wc.style            = 0;
-    wc.lpfnWndProc      = MainWndProc;
-    wc.cbClsExtra       = 0;
-    wc.cbWndExtra       = 0;
-    wc.hInstance        = h_inst;
-    wc.hIcon            = NULL;
-    wc.hCursor          = NULL;
-    wc.hbrBackground    = NULL;
-    wc.lpszClassName    = "ORS_MAIN_WINDOW_CLASS";
-    wc.lpszMenuName     = NULL;
-    wc.hIconSm          = NULL;
-
-    ATOM a = RegisterClassEx(&wc);
-    HWND h_wnd = CreateWindowEx(0,
-                                "ORS_MAIN_WINDOW_CLASS",
-                                "Test",
-                                WS_OVERLAPPEDWINDOW,
-                                CW_USEDEFAULT, CW_USEDEFAULT,
-                                CW_USEDEFAULT, CW_USEDEFAULT,
-                                NULL, NULL,
-                                h_inst,
-                                NULL);
-
-    if(h_wnd == NULL) {
-        ERR_MSGOUT("HWND is null");
-    }
-
-    ERR_MSGOUT("HWND is OK");
-
-    ShowWindow(h_wnd, cmd_show_opt);
-    UpdateWindow(h_wnd);
-
-    // main pseudocode:
-    // Configuration obj. should be loaded
-    // SystemGroup should be made
-    quit = false;
-    MSG msg;
-    while ( ! quit ) {
-        if(PeekMessage(&msg, h_wnd, 0, 0, true)) {
-            if(msg.message == WM_DESTROY) {
-                quit = true;
-            }
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
+    MLInterface mli;
+    global_addr = &mli;
+    mli.createMainWindow(h_inst, cmd_show_opt);
+    while ( ! mli.shouldQuit() ) {
         // main loop:
         //  - look for input, process if available
+        mli.emptyMessagePump();
         //  - update
         //  - render
     }
